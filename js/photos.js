807670a7feef8553aad2e75cca98ce49d2d060ba
shoumikahmed <shoumikahmed103@gmail.com>
2022-02-22 19:51:47 +0600

    photochild

--- js/photos.js
+++ js/photos.js
@@ -3,3 +3,16 @@ function loadPhotos() {
         .then(res => res.json())
         .then(data => displayPhotos(data))
 }
+loadPhotos()
+
+function displayPhotos(photos) {
+    const photosContainer = document.getElementById('photos')
+    for (const photo of photos) {
+        const div = document.createElement('div')
+        div.innerHTML = `
+        <p>${photo.url}</p>
+        <p>${photo.thumbnailUrl}</p>
+        `
+        photosContainer.appendChild(div)
+    }
+}
